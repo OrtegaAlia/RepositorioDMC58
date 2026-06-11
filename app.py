6bab1254385abb4cb742e8fdd6d4ee3441fda7e4
OrtegaAlia <ortegaalia13@gmail.com>
2026-06-11 18:30:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Alia Ortega")
+
+sesion = st.selectbox("Selecione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])
