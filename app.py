247351e9fafb72e8d496a7d4858898f74b172b30
OrtegaAlia <ortegaalia13@gmail.com>
2026-06-11 20:09:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ sesion = st.sidebar.selectbox("Selecione una sesión", ["Sesión 1", "Sesión 2"
 if sesion == "Sesión 1":
   st.write("Bienvenido la sesión 1")
 
-elif sesion == "Sesión 2"
+elif sesion == "Sesión 2":
   st.write("Bienvenido la sesión 2")
 
-elif sesion == "Sesión 3"
+elif sesion == "Sesión 3":
   st.write("Bienvenido la sesión 3")
 
 else:
