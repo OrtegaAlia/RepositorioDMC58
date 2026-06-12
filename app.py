74b7ab514292e813a7f95bf21e437ec6fd1209bf
OrtegaAlia <ortegaalia13@gmail.com>
2026-06-11 20:49:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Alia Ortega")
 
+st.sidebar.image("DMC.png")
+
 sesion = st.sidebar.selectbox("Selecione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])
 
 if sesion == "Sesión 1":
