1ebcde100aa39fa3541b0a22462fd66539a3c484
OrtegaAlia <ortegaalia13@gmail.com>
2026-06-11 20:07:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Alia Ortega")
 
 sesion = st.sidebar.selectbox("Selecione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])
+
+if sesion == "Sesión 1":
+  st.write("Bienvenido la sesión 1")
+
+elif sesion == "Sesión 2"
+  st.write("Bienvenido la sesión 2")
+
+elif sesion == "Sesión 3"
+  st.write("Bienvenido la sesión 3")
+
+else:
+  st.write("Bienvenido la sesión 4")
