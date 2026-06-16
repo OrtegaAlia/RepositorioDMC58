4cc6c6be8a507a37a6c62bc9fb77707afae6a036
OrtegaAlia <ortegaalia13@gmail.com>
2026-06-15 21:20:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,13 @@ else:
   archivo = st.sidebar.file_uploader("Cargue su archivo")
 
     if archivo is not None:
-    if archivo.name.endswith(".csv"):
-      datos = pd.read_csv(archivo)
-    elif archivo.name.endswith(".xlsx"):
-      datos = pd.read_excel(archivo)
+  
+      if archivo.name.endswith(".csv"):
+        datos = pd.read_csv(archivo)
+      elif archivo.name.endswith(".xlsx"):
+        datos = pd.read_excel(archivo)
 
     st.write(datos)
 
-  else: 
-    st.write("Cargue el archivo ")
+    else: 
+      st.write("Cargue el archivo ")
